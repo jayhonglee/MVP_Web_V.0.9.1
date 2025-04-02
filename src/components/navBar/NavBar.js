0aea845a825358f7838396bc460981625f6fba0b
Jaehong Lee <ja34luv@gmail.com>
2025-04-02 11:30:25 -0700

    Update

--- src/components/navBar/NavBar.js
+++ src/components/navBar/NavBar.js
@@ -1,16 +1,28 @@
 import logo from "../../assets/logo.png";
+import appStoreIcon from "../../assets/app-store.svg";
+import playStoreIcon from "../../assets/play-store.svg";
 
 function NavBar() {
   return (
     <div className="relative">
+      {/* Default Navbar Placeholder*/}
+      <div className="w-full h-[4rem] mobile:h-[79px] bg-transparent mobile:flex" />
+
       {/* Mobile Navbar */}
-      <div className="w-full h-[4rem] px-[14px] py-[17px] flex justify-between mobile:hidden">
+      <div className="fixed top-0 left-0 right-0 w-full h-[4rem] px-[14px] py-[17px] flex justify-between shadow-sm mobile:hidden">
         <a href="/">
           <img src={logo} alt="logo" className="h-full w-auto" />
         </a>
         <div className="flex items-center mr-[6px]">
           <div className="mr-[20px] bg-[#F43630] rounded-full px-[15px] py-[5px]">
-            <p className="text-sm text-white font-medium">App Coming Soon</p>
+            <p className="text-sm text-white font-medium flex items-center">
+              <img
+                src={appStoreIcon}
+                alt="App Store"
+                className="w-5 h-5 mr-1"
+              />
+              App Coming Soon
+            </p>
           </div>
           <div className="w-[16px] h-[14px] flex flex-col justify-between">
             <span className="w-full h-[2px] bg-[#383535]"></span>
@@ -19,9 +31,43 @@ function NavBar() {
           </div>
         </div>
       </div>
+
       {/* Desktop Navbar */}
-      <div className="fixed top-0 left-0 right-0 w-full h-[74.4px] pt-[28.4px] px-[14px] pb-[17px] flex justify-between bg-[#e6e1e1] hidden mobile:flex z-50" />
-      <div className="w-full h-[74.4px] pt-[28.4px] px-[14px] pb-[17px] bg-[#e6e1e1] hidden mobile:flex" />
+      <div className="fixed top-0 left-0 right-0 w-full h-[79px] bg-[#e6e1e1] hidden mobile:flex z-50 shadow-sm">
+        <div className="w-[1253px] h-[74.4px] mx-auto pt-[28.4px] px-[14px] pb-[17px] flex justify-center items-center">
+          <a href="/" className="text-2xl font-bold">
+            HANGOUT
+          </a>
+          <div className="w-[1055px] h-full flex justify-between items-center">
+            <div className="flex items-center">
+              <a href="/" className="font-[600] text-[18px] ms-[40px]">
+                Drop-In
+              </a>
+              <a href="/" className="font-[600] text-[18px] ms-[40px]">
+                Group Chat
+              </a>
+              <a href="/" className="font-[600] text-[18px] ms-[40px]">
+                Profile
+              </a>
+              <a href="/" className="font-[600] text-[18px] ms-[40px]">
+                Create / Edit Drop-In
+              </a>
+            </div>
+            <div>
+              <div className="bg-[#F43630] rounded-full px-[15px] py-[5px]">
+                <p className="text-sm text-white font-medium flex items-center">
+                  <img
+                    src={appStoreIcon}
+                    alt="App Store"
+                    className="w-5 h-5 mr-1"
+                  />
+                  App Coming Soon
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
