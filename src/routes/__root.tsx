1d6f968559408ddd57ff195e112e666a9fd4fc48
Jaehong Lee <ja34luv@gmail.com>
2025-04-25 13:43:51 -0700

    update

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,16 @@
 import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { Route as IndexRoute } from "./index";
+import { Route as LoginRoute } from "./login";
+import { Route as DropinRoute } from "./dropin";
+import { Route as CreateDropinRoute } from "./createDropin";
 
 export const Route = createRootRoute({
   component: () => <Outlet />,
 });
+
+export const routeTree = Route.addChildren([
+  IndexRoute,
+  LoginRoute,
+  DropinRoute,
+  CreateDropinRoute,
+]);
