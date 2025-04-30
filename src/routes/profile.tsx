a1a9a7ab6732607538886e80c04fd298bbbd21c9
Jaehong Lee <ja34luv@gmail.com>
2025-04-30 15:54:51 -0700

    update

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, Outlet } from "@tanstack/react-router";
 import NavBar from "../components/NavBar";
 
 export const Route = createFileRoute("/profile")({
@@ -13,17 +13,17 @@ function RouteComponent() {
 
   return (
     <div className="w-full h-[731px] p-[64px_14px_0] min-[600px]:h-[747px] mobile:h-[859px] mobile:p-[92px_0_0_0] flex flex-col justify-start items-center">
-      <div className="w-full">
+      <div className="w-full flex justify-center items-center">
         {/* Navbar */}
         <NavBar currentPage="Profile" noPlaceholder />
 
-        <div className="w-full max-w-[1200px] flex flex-col justify-start items-start mobile:flex-row mobile:justify-center mobile:gap-[27px] mobile:m-auto mobile:min-w-[1200px] mobile:overflow-hidden">
+        <div className="w-full max-w-[1200px] flex flex-col justify-start items-start mobile:flex-row mobile:justify-center mobile:gap-[27px] mobile:min-w-[1200px]">
           {/* < 900 px tabs */}
           <div className="w-full flex justify-start items-center gap-[20px] mobile:hidden overflow-hidden">
             {tabs.map((tab) => (
               <div
                 key={tab}
-                className={`h-[32px] flex justify-center items-center pb-[8px] border-b-[2px] border-[#383535] overflow-hidden shrink-0 whitespace-nowrap ${
+                className={`h-[32px] flex justify-center items-center pb-[8px] border-b-[2px] border-[#383535] overflow-hidden shrink-0 whitespace-nowrap cursor-pointer ${
                   activeTab === tab
                     ? "border-b-[#383535] text-[#383535]"
                     : "border-b-transparent text-[#B9B6B6]"
@@ -40,18 +40,22 @@ function RouteComponent() {
           {/* > 900 px tabs */}
           <div className="w-[293.250px] flex-col justify-start overflow-hidden hidden mobile:block pl-[10px] shrink-0">
             {tabs.map((tab) => (
-              <>
+              <div
+                key={tab}
+                className="cursor-pointer"
+                onClick={() => setActiveTab(tab)}
+              >
                 <p className="text-[20px] font-[500] leading-[77px] tracking-[-0.25px] text-[rgb(56,53,53)]">
                   {tab}
                 </p>
                 <div className="w-full max-w-[640px] min-w-[1px] h-[1px] bg-[#F4F4F4]" />
-              </>
+              </div>
             ))}
           </div>
 
           {/* tabs */}
-          <div className="w-full h-[120px] mobile:pr-[10px]">
-            <div className="w-full h-[120px] bg-red-500"></div>
+          <div className="w-full mobile:pr-[10px]">
+            <Outlet />
           </div>
         </div>
       </div>
