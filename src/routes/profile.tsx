46ceb3a032cce2469b90c5dee6bae728638e29ea
Jaehong Lee <ja34luv@gmail.com>
2025-05-02 23:42:49 -0700

    update

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,16 +1,21 @@
 import { useState } from "react";
-import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { createFileRoute, Outlet, useRouter } from "@tanstack/react-router";
 import NavBar from "../components/NavBar";
 
 export const Route = createFileRoute("/profile")({
   component: RouteComponent,
 });
 
-const tabs = ["Profile", "Group Chat", "Settings", "Logout"];
+const tabs = [
+  { name: "Profile", path: "" },
+  { name: "Group Chat", path: "group-chat" },
+  { name: "Settings", path: "settings" },
+  { name: "Logout", path: "logout" },
+];
 
 function RouteComponent() {
   const [activeTab, setActiveTab] = useState(tabs[0]);
-
+  const router = useRouter();
   return (
     <div className="w-full h-[731px] p-[64px_14px_0] min-[600px]:h-[747px] mobile:h-[859px] mobile:p-[92px_0_0_0] flex flex-col justify-start items-center">
       <div className="w-full flex justify-center items-center">
@@ -22,16 +27,19 @@ function RouteComponent() {
           <div className="w-full flex justify-start items-center gap-[20px] mobile:hidden overflow-hidden">
             {tabs.map((tab) => (
               <div
-                key={tab}
+                key={tab.path}
                 className={`h-[32px] flex justify-center items-center pb-[8px] border-b-[2px] border-[#383535] overflow-hidden shrink-0 whitespace-nowrap cursor-pointer ${
                   activeTab === tab
                     ? "border-b-[#383535] text-[#383535]"
                     : "border-b-transparent text-[#B9B6B6]"
                 }`}
-                onClick={() => setActiveTab(tab)}
+                onClick={() => {
+                  setActiveTab(tab);
+                  router.navigate({ to: `/profile/${tab.path}` });
+                }}
               >
                 <p className="text-[16px] font-[500] leading-[24px] tracking-[-0.25px]">
-                  {tab}
+                  {tab.name}
                 </p>
               </div>
             ))}
@@ -41,12 +49,15 @@ function RouteComponent() {
           <div className="w-[293.250px] flex-col justify-start overflow-hidden hidden mobile:block pl-[10px] shrink-0">
             {tabs.map((tab) => (
               <div
-                key={tab}
+                key={tab.path}
                 className="cursor-pointer"
-                onClick={() => setActiveTab(tab)}
+                onClick={() => {
+                  setActiveTab(tab);
+                  router.navigate({ to: `/profile/${tab.path}` });
+                }}
               >
                 <p className="text-[20px] font-[500] leading-[77px] tracking-[-0.25px] text-[rgb(56,53,53)]">
-                  {tab}
+                  {tab.name}
                 </p>
                 <div className="w-full max-w-[640px] min-w-[1px] h-[1px] bg-[#F4F4F4]" />
               </div>
