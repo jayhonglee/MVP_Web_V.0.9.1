83c60017f052ab7e15a99196b285384ef2c4d3f0
Jaehong Lee <ja34luv@gmail.com>
2025-05-30 21:38:17 -0700

    update

--- src/routes/profile/chat-room.tsx
+++ src/routes/profile/chat-room.tsx
@@ -50,7 +50,7 @@ function ChatRoom() {
           </div>
         </div>
         {/* Right side */}
-        <div className="flex justify-end items-center gap-[12px] h-[44px] cursor-pointer">
+        <div className="flex justify-end items-center gap-[12px] h-[44px]">
           {getChatRoomIcon("info")}
         </div>
       </div>
@@ -60,28 +60,13 @@ function ChatRoom() {
 
       {/* Chat room input */}
       <div className="w-full h-[78px] flex flex-col justify-center items-center p-[16px]">
-        <div className="w-full h-[46px] border-[1px] border-gray-200 rounded-[23px] flex justify-center items-center pl-[11px] pr-[16px]">
-          <div
-            contentEditable
-            className="w-full h-[20px] outline-none text-[15px] font-[400] leading-[13px] break-words flex items-center"
-            role="textbox"
-            aria-label="Message input"
-            data-placeholder="Message..."
-            onBlur={(e) => {
-              if (!e.currentTarget.textContent) {
-                e.currentTarget.textContent = "Message...";
-                e.currentTarget.classList.add("text-gray-400");
-              }
-            }}
-            onFocus={(e) => {
-              if (e.currentTarget.textContent === "Message...") {
-                e.currentTarget.textContent = "";
-                e.currentTarget.classList.remove("text-gray-400");
-              }
-            }}
-            suppressContentEditableWarning
+        <div className="w-full h-[46px] border-[1px] border-gray-200 rounded-[23px] flex justify-center items-center px-[16px]">
+          <input
+            type="text"
+            className="w-full h-[20px] outline-none text-[15px] font-[400] leading-[18px] break-words flex items-center"
+            placeholder="Message..."
           />
-          <p className="text-[14px] leading-[18px] text-[rgb(0,149,246)] font-[600] ml-[5px] cursor-pointer">
+          <p className="text-[14px] leading-[18px] text-[rgb(0,149,246)] font-[600] ml-[5px]">
             Send
           </p>
         </div>
